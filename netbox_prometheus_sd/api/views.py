3980d4275fa86cc0b70cec995b13a3df347e2d03
Felix Peters <felix.peters@breuninger.de>
2021-05-30 23:54:01 +0200

    Add basic output to api from database

--- netbox_prometheus_sd/api/views.py
+++ netbox_prometheus_sd/api/views.py
@@ -6,6 +6,7 @@ from .serializers import TargetSerializer
 from . import Target
 
 from ipam.models import IPAddress
+from virtualization.models import VirtualMachine
 
 
 class TargetViewSet(viewsets.ViewSet):
@@ -16,10 +17,23 @@ class TargetViewSet(viewsets.ViewSet):
     def list(self, request):
         data = []
 
+        # Todo: refactor to testable mapper class
+        for vm in VirtualMachine.objects.filter(primary_ip4__isnull=False).all():
+            target = Target(str(vm.primary_ip))
+            target.add_label("__meta_netbox_type", "virtual_machine")
+            target.add_label("__meta_netbox_cluster", vm.cluster.name)
+            if getattr(vm, "tenant", None):
+                target.add_label("__meta_netbox_tenant", vm.tenant.name)
+                target.add_label("__meta_netbox_tenant_slug", vm.tenant.slug)
+                if vm.tenant.group:
+                    target.add_label("tenant_group", vm.tenant.group.name)
+                    target.add_label("tenant_group_slug", vm.tenant.group.slug)
+            data.append(target)
+
         for ip in IPAddress.objects.all():
-            t = Target(str(ip))
-            t.add_label("foo", "bar")
-            data.append(t)
+            target = Target(str(ip))
+            target.add_label("__meta_netbox_type", "ip")
+            data.append(target)
 
         serializer = TargetSerializer(instance=data, many=True)
 
--- netbox_prometheus_sd/tests/test_api.py
+++ netbox_prometheus_sd/tests/test_api.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 
-
 from ipam.models import IPAddress
 from tenancy.models import Tenant
 from virtualization.models import Cluster, ClusterType, VirtualMachine, VMInterface
@@ -29,11 +28,13 @@ class AppMetricEndpointTests(TestCase):
             name="Default Cluster", type=cluster_type, site=site
         )
 
-        vm = VirtualMachine.objects.create(
-            name="Instance-01", cluster=cluster, tenant=tenant
-        )
+        self.create_vm("Instance-01", "10.10.10.10/24", cluster=cluster, tenant=tenant)
+        self.create_vm("Instance-02", "10.10.10.11/24", cluster=cluster, tenant=tenant)
 
-        ip = IPAddress.objects.create(address="10.10.10.10/24")
+    def create_vm(self, name, address, cluster, tenant):
+        vm = VirtualMachine.objects.create(name=name, cluster=cluster, tenant=tenant)
+
+        ip = IPAddress.objects.create(address=address)
         interface = VMInterface.objects.create(virtual_machine=vm, name="default")
 
         vm_interface_ct = ContentType.objects.get_for_model(VMInterface)
@@ -49,3 +50,9 @@ class AppMetricEndpointTests(TestCase):
         """Ensure the endpoint is working properly and is not protected by authentication."""
         resp = self.client.get(self.url)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_debug(self):
+        """ Debug, remove later"""
+        obj = VirtualMachine.objects.all()
+        print(obj)
+        self.assertEqual(2, len(obj))
