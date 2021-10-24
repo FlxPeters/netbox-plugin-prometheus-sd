2baae24d627a0425fe0ad87b7d3e9e92ca68698b
Felix Peters <felix.peters@breuninger.de>
2021-10-24 20:02:11 +0200

    refactor tests to create objects in DB

--- netbox_prometheus_sd/tests/test_mapper.py
+++ netbox_prometheus_sd/tests/test_mapper.py
@@ -1,6 +1,8 @@
+from os import name
+import re
 from django.test import TestCase
 from django.test.utils import tag
-from dcim.models.devices import DeviceType
+from dcim.models.devices import DeviceType, Manufacturer
 from dcim.models.sites import Site
 from netbox_prometheus_sd.api.models import Target, TargetType
 
@@ -13,53 +15,81 @@ from tenancy.models import Tenant, TenantGroup
 from virtualization.models import Cluster, ClusterGroup, ClusterType, VirtualMachine
 
 
+def build_cluster():
+
+    return Cluster.objects.create(
+        name="DC1",
+        group=ClusterGroup.objects.create(name="VMware"),
+        type=ClusterType.objects.create(name="On Prem"),
+        site=Site.objects.create(name="Campus A", slug="campus-a"),
+    )
+
+
+def build_tenant():
+    return Tenant.objects.create(name="Acme Corp.", slug="acme")
+
+
+def build_device_role():
+    return DeviceRole.objects.create(name="Core Switch", slug="core-switch")
+
+
 def build_minimal_vm():
-    return VirtualMachine(name="vm-01.example.com")
+    return VirtualMachine.objects.create(
+        name="vm-01.example.com", cluster=build_cluster()
+    )
 
 
 def build_vm_full():
-    return VirtualMachine(
+    return VirtualMachine.objects.create(
         name="vm-full-01.example.com",
-        cluster=Cluster(
-            name="DC1",
-            group=ClusterGroup(name="VMware"),
-            type=ClusterType(name="On Prem"),
-            site=Site(name="Campus A", slug="campus-a"),
-        ),
-        tenant=Tenant(name="Acme Corp.", slug="acme"),
-        role=DeviceRole(name="VM", slug="vm", vm_role=True),
-        platform=Platform(name="Ubuntu 20.04", slug="ubuntu-20.04"),
-        primary_ip4=IPAddress(address="192.168.0.1/24"),
+        cluster=build_cluster(),
+        tenant=build_tenant(),
+        role=DeviceRole.objects.create(name="VM", slug="vm", vm_role=True),
+        platform=Platform.objects.create(name="Ubuntu 20.04", slug="ubuntu-20.04"),
+        primary_ip4=IPAddress.objects.create(address="192.168.0.1/24"),
     )
 
 
 def build_minimal_device():
-    return Device(name="core-switch-01")
+    return Device.objects.create(
+        name="core-switch-01",
+        device_role=build_device_role(),
+        device_type=DeviceType.objects.create(
+            model="Firewall Device",
+            slug="firewall",
+            manufacturer=Manufacturer.objects.create(name="Cisco", slug="cisco"),
+        ),
+        site=Site.objects.create(name="Site Device", slug="site-device"),
+    )
 
 
 def build_device_full():
-    return Device(
+    return Device.objects.create(
         name="core-switch-full-01",
-        tenant=Tenant(name="Acme Corp.", slug="acme"),
-        device_role=DeviceRole(name="Core Switch", slug="core-switch"),
-        platform=Platform(name="Junos", slug="junos"),
-        site=Site(name="Campus A", slug="campus-a"),
-        device_type=DeviceType(model="SRX", slug="srx"),
-        primary_ip6=IPAddress(address="2001:db8:1701::2/64"),
+        tenant=build_tenant(),
+        device_role=build_device_role(),
+        platform=Platform.objects.create(name="Junos", slug="junos"),
+        site=Site.objects.create(name="Campus B", slug="campus-b"),
+        device_type=DeviceType.objects.create(
+            model="SRX",
+            slug="srx",
+            manufacturer=Manufacturer.objects.create(name="Juniper", slug="juniper"),
+        ),
+        primary_ip6=IPAddress.objects.create(address="2001:db8:1701::2/64"),
     )
 
 
 def build_minimal_ip():
-    return IPAddress(address="10.10.10.10/24")
+    return IPAddress.objects.create(address="10.10.10.10/24")
 
 
 def build_full_ip():
-    return IPAddress(
+    return IPAddress.objects.create(
         address="10.10.10.10/24",
-        tenant=Tenant(
+        tenant=Tenant.objects.create(
             name="Starfleet",
             slug="starfleet",
-            group=TenantGroup(name="Federation", slug="federation"),
+            group=TenantGroup.objects.create(name="Federation", slug="federation"),
         ),
         dns_name="foo.example.com",
     )
@@ -133,9 +163,9 @@ class MapperTests(TestCase):
         self.assertDictContainsSubset(
             {"__meta_netbox_platform_slug": "junos"}, target.labels
         )
-        self.assertDictContainsSubset({"__meta_netbox_site": "Campus A"}, target.labels)
+        self.assertDictContainsSubset({"__meta_netbox_site": "Campus B"}, target.labels)
         self.assertDictContainsSubset(
-            {"__meta_netbox_site_slug": "campus-a"}, target.labels
+            {"__meta_netbox_site_slug": "campus-b"}, target.labels
         )
         self.assertDictContainsSubset(
             {"__meta_netbox_ip": "2001:db8:1701::2"}, target.labels
