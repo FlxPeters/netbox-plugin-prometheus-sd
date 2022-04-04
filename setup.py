486144c55681be7341fc251fe2f091666495df76
andreip <andrei.protsenko@adyen.com>
2022-04-04 13:49:49 +0200

    fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="netbox_prometheus_sd",
-    version="0.2.1",
+    version="0.3",
     description="Netbox plugin which provide API for prometheus HTTP Service Discovery",
     url="https://github.com/zelfix/netbox-prometheus-sd",
     author="Andrei Protsenko",
