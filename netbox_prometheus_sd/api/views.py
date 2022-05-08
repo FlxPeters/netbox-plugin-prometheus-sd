3ef2d9a93f71f91a0ea67828c2d8e21c8a07cc7c
Stephan Jaekel <steph@rdev.info>
2022-05-08 19:52:45 +0200

    Ensure imports work for Netbox <3.2 and >=3.2.

--- netbox_prometheus_sd/api/views.py
+++ netbox_prometheus_sd/api/views.py
@@ -1,7 +1,14 @@
 from ipam.models import IPAddress
 from virtualization.models import VirtualMachine
 from dcim.models.devices import Device
-from netbox.api.viewsets import NetBoxModelViewSet
+
+# The base ViewSet has been renamed, this try-except helps to support
+# Both < 3.2 and the newer 3.2+ Versions:
+# https://github.com/netbox-community/netbox/commit/bbdeae0ed9bcc06fb96ffa2970272e1a3447448c
+try:
+    from netbox.api.viewsets import NetBoxModelViewSet
+except ImportError:
+    from extras.api.views import CustomFieldModelViewSet as NetBoxModelViewSet
 
 # Filtersets have been renamed, we support both
 # https://github.com/netbox-community/netbox/commit/1024782b9e0abb48f6da65f8248741227d53dbed#diff-d9224204dab475bbe888868c02235b8ef10f07c9201c45c90804d395dc161c40
