d55c9b9d9fd26f9e5c24e55de8dbe61ee803d243
andreip <andrei.protsenko@adyen.com>
2022-08-19 07:15:41 +0200

    Add compbability with netbox v3.2

--- netbox_prometheus_sd/api/views.py
+++ netbox_prometheus_sd/api/views.py
@@ -1,12 +1,12 @@
 from dcim.models.devices import Device
-from extras.api.views import CustomFieldModelViewSet
+from netbox.api.viewsets import NetBoxModelViewSet
 from netbox_prometheus_sd.settings import CUSTOM_FIELD_NAME
 from dcim.filtersets import DeviceFilterSet
 from .serializers import PrometheusDeviceSerializer
 
 
-class DeviceViewSet(CustomFieldModelViewSet):
-    queryset = Device.objects.filter(custom_field_data={CUSTOM_FIELD_NAME: True})
+class DeviceViewSet(NetBoxModelViewSet):
+    queryset = Device.objects.filter(custom_field_data__contains={CUSTOM_FIELD_NAME: True})
     filterset_class = DeviceFilterSet
     serializer_class = PrometheusDeviceSerializer
     pagination_class = None
