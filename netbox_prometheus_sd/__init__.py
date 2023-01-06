ea6ee219e88a479e31a843732386cfc4038ab854
Ryan Merolle <ryanmerolle@users.noreply.github.com>
2023-01-06 16:39:24 -0500

    Correct __init__ listed version

--- netbox_prometheus_sd/__init__.py
+++ netbox_prometheus_sd/__init__.py
@@ -7,7 +7,7 @@ class PrometheusSD(PluginConfig):
     description = (
         "Provide Prometheus url_sd compatible API Endpoint with data from netbox"
     )
-    version = "0.4"
+    version = "0.5"
     author = "Felix Peters"
     author_email = "mail@felixpeters.de"
     base_url = "prometheus-sd"
