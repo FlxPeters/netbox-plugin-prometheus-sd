0dfd05c4cf8d38e8f1a2ca0f45ab971839fb84cc
Felix Peters <felix.peters@breuninger.de>
2023-02-22 22:50:05 +0100

    chore: update project dependencies to fix tests Drop older Netbox versions in tests

--- develop/configuration.py
+++ develop/configuration.py
@@ -1,6 +1,5 @@
 """NetBox configuration."""
 import os
-from distutils.util import strtobool
 from django.core.exceptions import ImproperlyConfigured
 
 # Enforce required configuration parameters
@@ -16,6 +15,21 @@ for key in [
 ]:
     if not os.environ.get(key):
         raise ImproperlyConfigured(f"Required environment variable {key} is missing.")
+
+
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    if val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+
+    raise ValueError(f"invalid truth value {val}")
 
 
 def is_truthy(arg):
