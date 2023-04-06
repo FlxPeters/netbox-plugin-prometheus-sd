68ad01ff4a1cacad3667ce3e318f0104d619295f
geg347 <geg347@users.noreply.gihub.com>
2023-04-06 15:28:43 +0200

    Fixing unit test for rack label
    Co-authored-by: Arthur Outhenin-Chalandre <arthur.outhenin-chalandre@proton.ch>

--- netbox_prometheus_sd/tests/utils.py
+++ netbox_prometheus_sd/tests/utils.py
@@ -99,7 +99,9 @@ def build_device_full(name):
     device.primary_ip6 = IPAddress.objects.get_or_create(address="2001:db8:1701::2/64")[
         0
     ]
-    device.rack = Rack.objects.get_or_create(name="R01B01")[0]
+    device.rack = Rack.objects.get_or_create(
+        name="R01B01", site=Site.objects.get_or_create(name="Site", slug="site")[0]
+    )[0]
     device.tags.add("Tag1")
     device.tags.add("Tag 2")
     return device
