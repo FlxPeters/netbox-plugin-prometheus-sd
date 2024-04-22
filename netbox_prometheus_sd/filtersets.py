43a832cf3b16a8cacf61cac5c3431c67629bf956
Jakub Krcma <jakub.krcma@proton.ch>
2024-04-22 17:57:05 +0200

    feat: support tenant filtering in services
    This change adds support for tenancy filtering in the services API
    endpoint. Apart from services, all other models already support such
    filtering. As the tenancy support in Netbox is inconsistent or
    incomplete, a manual FilterSet had to be implemented.

---
+++ netbox_prometheus_sd/filtersets.py
@@ -0,0 +1,49 @@
+# Filtersets have been renamed, we support both
+# https://github.com/netbox-community/netbox/commit/1024782b9e0abb48f6da65f8248741227d53dbed#diff-d9224204dab475bbe888868c02235b8ef10f07c9201c45c90804d395dc161c40
+from django.db.models import Q
+from django.utils.translation import gettext as _
+
+from utilities.filters import MultiValueCharFilter, MultiValueNumberFilter
+
+try:
+    from ipam.filtersets import ServiceFilterSet as NetboxServiceFilterSet
+except ImportError:
+    from ipam.filters import ServiceFilterSet as NetboxServiceFilterSet
+
+
+class ServiceFilterSet(NetboxServiceFilterSet):
+    """Filter set to support tenancy over the device/VM foreign key.
+
+    Tenancy in Netbox is very incosistent and the relationship on its own is defined across many different models. This
+    means that supporting all layers is nearly impossible without a stronger upstream support. For this reason only the
+    "first level" tenancy is supported by this filter set.
+    """
+
+    tenant_id = MultiValueNumberFilter(
+        method='filter_by_tenant_id',
+        label=_('Tenant (ID)'),
+    )
+
+    tenant = MultiValueCharFilter(
+        method='filter_by_tenant_slug',
+        label=_('Tenant (slug)'),
+    )
+
+    # pylint: disable=unused-argument
+    def filter_by_cluster_tenant_id(self, queryset, name, value):
+        return queryset.filter(
+            Q(device__cluster__tenant_id__in=value) |
+            Q(virtual_machine__cluster__tenant_id__in=value)
+        )
+
+    def filter_by_cluster_tenant_slug(self, queryset, name, value):
+        return queryset.filter(
+            Q(device__cluster__tenant__slug__in=value) |
+            Q(virtual_machine__cluster__tenant__slug__in=value)
+        )
+
+    def filter_by_tenant_id(self, queryset, name, value):
+        return queryset.filter(Q(device__tenant_id__in=value) | Q(virtual_machine__tenant_id__in=value))
+
+    def filter_by_tenant_slug(self, queryset, name, value):
+        return queryset.filter(Q(device__tenant__slug__in=value) | Q(virtual_machine__tenant__slug__in=value))
---
+++ netbox_prometheus_sd/tests/test_filtersets.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+
+from ipam.models import Service
+from tenancy.models import Tenant
+from utilities.testing import ChangeLoggedFilterSetTests
+
+from . import utils
+from ..filtersets import ServiceFilterSet
+
+
+class ServiceTestCase(TestCase, ChangeLoggedFilterSetTests):
+    queryset = Service.objects.all()
+    filterset = ServiceFilterSet
+
+    @classmethod
+    def setUpTestData(cls):
+        """Netbox requires us to define test data in this method, otherwise the ORM won't pick them.
+        """
+        for i in range(1, 4):
+            utils.build_device_full(f"firewall-full-0{i}", i)
+            utils.build_vm_full(f"vm-full-0{i}.example.com", i)
+
+    def test_device_tenant(self):
+        tenant = Tenant.objects.all()[0]
+
+        params = {'tenant_id': [tenant.pk]}
+        self.assertEqual(self.filterset(params, self.queryset).qs.count(), 6)
+        params = {'tenant': [tenant.slug]}
+        self.assertEqual(self.filterset(params, self.queryset).qs.count(), 6)
+
+    def test_vm_tenant(self):
+        tenant = Tenant.objects.all()[0]
+
+        params = {'tenant_id': [tenant.pk]}
+        self.assertEqual(self.filterset(params, self.queryset).qs.count(), 6)
+        params = {'tenant': [tenant.slug]}
+        self.assertEqual(self.filterset(params, self.queryset).qs.count(), 6)
