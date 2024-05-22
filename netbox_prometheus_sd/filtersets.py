20606aca6cf93709bae203cd1196bf0d1d91913f
Felix Peters <felix.peters@breuninger.de>
2024-05-22 23:52:43 +0200

    feat: add netbox 4.0 compatible imports

--- netbox_prometheus_sd/filtersets.py
+++ netbox_prometheus_sd/filtersets.py
@@ -3,7 +3,7 @@
 from django.db.models import Q
 from django.utils.translation import gettext as _
 
-from utilities.filters import MultiValueCharFilter, MultiValueNumberFilter
+from utilities.filters import MultiValueCharFilter, MultiValueNumberFilter, NumericArrayFilter
 
 try:
     from ipam.filtersets import ServiceFilterSet as NetboxServiceFilterSet
@@ -27,6 +27,13 @@ class ServiceFilterSet(NetboxServiceFilterSet):
     tenant = MultiValueCharFilter(
         method='filter_by_tenant_slug',
         label=_('Tenant (slug)'),
+    )
+
+    # fix to make the test_missing_filters pass
+    # see: https://github.com/netbox-community/netbox/blob/master/netbox/utilities/testing/filtersets.py#L98
+    ports = NumericArrayFilter(
+        field_name='ports',
+        lookup_expr='contains'
     )
 
     # pylint: disable=unused-argument
